64e0679875c253e0cccc8383200ced9e212a527b
asobe <soa223@gmail.com>
2024-12-12 13:28:15 +0100

    updated as pipeline otherwise fails

--- comments.js
+++ comments.js
@@ -1,16 +1,8 @@
-//create a webserver
+// Create web server
 const http = require('http');
-const fs = require('fs');
-
 http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      return res.end('404 Not Found');
-    }
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end();
-  });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Hello World');
+  res.end();
 }).listen(8080);
+
